f9dfb013a5213dfa4c651414ac173797f395148a
xStef23 <trstefy@gmail.com>
2022-02-07 15:00:21 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [colour, setColour] = useState("#ffffff");
+  const randomizedColour = () => {
+    const randomColour =
+      "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+    setColour(randomColour);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor: `${colour}` }}>
+      <h1>Random Colours Generator</h1>
+      <button onClick={randomizedColour}>Generate a random colour</button>
+      <button
+        onClick={() => {
+          navigator.clipboard.writeText(colour);
+          //console.log(colour);
+        }}
+      >
+        Get the colour code
+      </button>
+
+      <h1>{colour}</h1>
     </div>
   );
-}
+};
 
 export default App;
